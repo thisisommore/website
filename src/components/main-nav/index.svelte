8d82165c235bf068d26167ea3c25083fc226bd3f
nisarhassan12 <syednisarhassan12@gmail.com>
2021-07-20 15:57:02 +0500

    add the indicator to the changelog instead of careers.

--- src/components/main-nav/index.svelte
+++ src/components/main-nav/index.svelte
@@ -27,11 +27,11 @@
     {
       href: "/changelog",
       label: "Changelog",
+      isHighlighted: true,
     },
     {
       href: "/careers",
       label: "We're hiring",
-      isHighlighted: true,
     },
   ];
 </script>
